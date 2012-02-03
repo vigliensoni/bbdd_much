fbe1b25ed16820ee4d8e1ad7b7231fe4088d86e8
Gabriel Vigliensoni <gabriel@vigliensoni.com>
2012-02-02 20:36:38 -0500

    fixed scrap_parser for properly extract the entries from a list separated by a <br> tag

--- scrap_parser.py
+++ scrap_parser.py
@@ -1,5 +1,5 @@
 import urllib2, re, os, pickle, sys, csv
-from BeautifulSoup import BeautifulSoup
+from BeautifulSoup import BeautifulSoup, NavigableString, Tag
 from optparse import OptionParser
 
 sys.setrecursionlimit(10000)
@@ -24,74 +24,114 @@ def PEOPLE_ARTIST_from_musicapopular_cl(input_folder, output_folder):
     # sys.stdout = writer
     for dirpath, dirnames, filenames in os.walk(input_folder):
         for f in filenames:
-             
-            if f.startswith(".") or f.endswith(".txt"):
-                continue
-            
-
-            file_path = os.path.join(dirpath, f)
-            g = open(file_path, 'rb')
-            page = pickle.load(g)
-
-            print 'http://www.musicapopular.cl/3.0/index2.php?op=Artista&id=&{0}'.format(f)
-
-            #ARTIST
-            artist = page.find('a', {'id':"up"}).text
-            # print artist
-
-            people = page.find('span', {'class':"boxficha_integrantes"}).text.split('.')
-
-            for p in people:
-                # print p
-                if len(p) is not 0:
-                    # PERSON
-                    p = p.strip('Integrantes:')
-                    p = p.strip('Grupos:')
-                    name = p.split(',')[0].split()
-                    f_name = [name[0]]
-                    l_name = name[1:]
-                    l_name = [' '.join(l_name)]
-
-                    # INSTRUMENTS
-                    instruments = []
-                    init = p.find(',')
-                    end = p.find('(')
-                    instrument_list = p[init + 1:end]
-                    instrument_list = instrument_list.replace(' y ', ',').split(',')
-                    for i in instrument_list:
-                        instruments.append(i.strip().capitalize())
 
 
-                    # YEARS
-                    init = [i for i, x in enumerate(p) if x == "("]
-                    end = [i for i, x in enumerate(p) if x == ")"]
+            try:           
+                if f.startswith(".") or f.endswith(".txt"):
+                    continue
+                
+
+                file_path = os.path.join(dirpath, f)
+                g = open(file_path, 'rb')
+                page = pickle.load(g)
+
+                print 'http://www.musicapopular.cl/3.0/index2.php?op=Artista&id=&{0}'.format(f)
+
+                #ARTIST
+                artist = page.find('a', {'id':"up"}).text
+                print artist
+
+
+                people = page.find('span', {'class':"boxficha_integrantes"})
+                # print people
+                ary = []
+                if people.findAll(text=True)[0].startswith('G'):   # For extracting groups for individuals
+                    artist_type = 1
+                    # br = people.findAll('br')
+                    # for b in br:  
+                    #     print b.findNext().string()              
+
+                elif people.findAll(text=True)[0].startswith('I'): # For extracting individuals from Groups
+                    artist_type = 2        
+
+
+                for br in people.findAll('br')[0:-1]:
+                    data = []
+                    next = br.nextSibling
+                    while not (isinstance(next, Tag) and next.name == 'br'):
+                        if isinstance(next, Tag) and next.name == 'em':
+                        # if next.find('em') and next.find('em') != -1:
+                            alias = next.find('em').string
+                            next.find('em').replaceWith(" ")
+                            data.append(next.name)
+                        else:
+                            data.append(next.string)
+                        next = next.nextSibling
+                    print data
+
+
+                else:
+                    artist_type = 0
+                
+                g.close()
+            except:
+                print 'PROBLEM IN PAGE {0}'.format(f)
+
+
+
+
+
+            # for p in people:
+            #     # print p
+            #     if len(p) is not 0:
+            #         # PERSON
+            #         p = p.strip('Integrantes:')
+            #         p = p.strip('Grupos:')
+            #         name = p.split(',')[0].split()
+            #         f_name = [name[0]]
+            #         l_name = name[1:]
+            #         l_name = [' '.join(l_name)]
+
+            #         # INSTRUMENTS
+            #         instruments = []
+            #         init = p.find(',')
+            #         end = p.find('(')
+            #         instrument_list = p[init + 1:end]
+            #         instrument_list = instrument_list.replace(' y ', ',').split(',')
+            #         for i in instrument_list:
+            #             instruments.append(i.strip().capitalize())
+
+
+            #         # YEARS
+            #         init = [i for i, x in enumerate(p) if x == "("]
+            #         end = [i for i, x in enumerate(p) if x == ")"]
                     
-                    try:
-                        from_year = p[init[-1]]
-                        to_year = p[end[-1]]
+            #         try:
+            #             from_year = p[init[-1]]
+            #             to_year = p[end[-1]]
 
-                        years = p[init[-1]+1:end[-1]]
-                        years = years.split('-')
-                        from_year = years[0].split()
-                        to_year = years[1].split()
-                    # except:
-                    #     print 'error in years'
-                    # try:
+            #             years = p[init[-1]+1:end[-1]]
+            #             years = years.split('-')
+            #             from_year = years[0].split()
+            #             to_year = years[1].split()
+            #         # except:
+            #         #     print 'error in years'
+            #         # try:
                         
-                    #     to_year = years[1].split()
-                    except:
-                        to_year = from_year
-                        print 'error in years'
+            #         #     to_year = years[1].split()
+            #         except:
+            #             to_year = from_year
+            #             print 'error in years'
 
-                    print artist, f_name, l_name, instruments, from_year, to_year
+            #         print artist, f_name, l_name, instruments, from_year, to_year
             
 
 
 
-            bio = page.find('div', {'id':"colcentral_bio"})
-            bio_creator = bio.find(text=re.compile("&mdash;")).split("&mdash;")[-1].lstrip(' ')
+            # bio = page.find('div', {'id':"colcentral_bio"})
+            # bio_creator = bio.find(text=re.compile("&mdash;")).split("&mdash;")[-1].lstrip(' ')
             
-            picture_creator = bio.find(text=re.compile("Foto:")).split("Foto:")[-1].lstrip(' ')
+            # picture_creator = bio.find(text=re.compile("Foto:")).split("Foto:")[-1].lstrip(' ')
 
             print '\n'
 
